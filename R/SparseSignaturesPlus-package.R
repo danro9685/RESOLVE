cedcbcef21bc4f45b69ee2b764bf3d3d45be46a7
danro9685 <daniele.ramazzotti@yahoo.com>
2022-10-16 12:54:57 +0200

    Adding a package-level man page

---
+++ R/SparseSignaturesPlus-package.R
@@ -0,0 +1,12 @@
+#' An R package for the efficient analysis of mutational signatures from cancer genomes.
+#' 
+#' This package provides a framework that allows for the efficient extraction and assignment of mutational signatures.
+#' It implements a novel algorithm that enables: (i) the efficient extraction, (ii) exposure estimation, and
+#' (iii) confidence assessment during the computational inference of mutational signatures.
+#' 
+#' @name SparseSignaturesPlus-package
+#' @aliases SparseSignaturesPlus
+#' @docType package
+#' @keywords package
+#' 
+NA
